bba31f6b1e62e24903cd2b4a9cf114a85872acd4
Divya Nair <divnairs@gmail.com>
2018-10-02 13:49:09 -0700

    record player answer selection

--- src/rounds/Answers.js
+++ src/rounds/Answers.js
@@ -1,17 +1,54 @@
-import React from "react";
-import shuffle from "lodash/shuffle";
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
 
-const getAllAnswers = obj => {
-  return shuffle([...obj.incorrect_answers, obj.correct_answer]);
-};
+const styles = theme => ({
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper
+  }
+});
 
-const Answers = props => {
-  return (
-    <section>
-      {props.results &&
-        props.results.map(result => <p>{getAllAnswers(result)}</p>)}
-    </section>
-  );
-};
+class Answers extends Component {
+  state = { selectedAnswer: null };
 
-export default Answers;
+  handleListItemClick = event => {
+    this.setState({
+      selectedAnswer: event.currentTarget.textContent
+    });
+  };
+
+  render() {
+    return (
+      <div className={this.props.classes.root}>
+        <List component="nav">
+          <ListItem button onClick={event => this.handleListItemClick(event)}>
+            <ListItemText primary="option1" />
+          </ListItem>
+          <Divider />
+
+          <ListItem button onClick={event => this.handleListItemClick(event)}>
+            <ListItemText primary="option2" />
+          </ListItem>
+          <Divider />
+
+          <ListItem button onClick={event => this.handleListItemClick(event)}>
+            <ListItemText primary="option3" />
+          </ListItem>
+          <Divider />
+
+          <ListItem button onClick={event => this.handleListItemClick(event)}>
+            <ListItemText primary="option4" />
+          </ListItem>
+        </List>
+        {console.log("STATE", this.state)}
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(Answers);
