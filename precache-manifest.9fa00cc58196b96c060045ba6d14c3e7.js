c84a56d52f0462d46101cf4bfba42111f032b6c4
Divya Nair <divnairs@gmail.com>
2018-12-13 14:52:28 -0800

    Updates

---
+++ precache-manifest.9fa00cc58196b96c060045ba6d14c3e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c296d2ff758f4c374fb5183608a3a9f",
+    "url": "/trivia-game/static/media/sports.5c296d2f.jpeg"
+  },
+  {
+    "revision": "26891a749e706aa06d9295be30e9088e",
+    "url": "/trivia-game/static/media/general.26891a74.jpeg"
+  },
+  {
+    "revision": "75883f0a7329d06235e5",
+    "url": "/trivia-game/static/js/runtime~main.75883f0a.js"
+  },
+  {
+    "revision": "6652a25a60c1ef2bd813",
+    "url": "/trivia-game/static/js/main.6652a25a.chunk.js"
+  },
+  {
+    "revision": "cac0735fcb18ba3aaefc",
+    "url": "/trivia-game/static/js/1.cac0735f.chunk.js"
+  },
+  {
+    "revision": "6652a25a60c1ef2bd813",
+    "url": "/trivia-game/static/css/main.62dc68b5.chunk.css"
+  },
+  {
+    "revision": "8909b8d37500607f032a8325f2087d36",
+    "url": "/trivia-game/index.html"
+  }
+];
